de3608b53f8ba969874caef9037a6a1612fb2b5e
Maicon8Matos <maicondossantosmatos1995@gmail.com>
2025-10-04 19:11:20 -0400

    EndPoint: find Category by Id and Excandeption handling

--- backend/src/main/java/com/example/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/example/dscatalog/resources/CategoryResource.java
@@ -1,12 +1,12 @@
 package com.example.dscatalog.resources;
 
 import com.example.dscatalog.dtos.CategoryDto;
-import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.services.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +23,10 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDto>> findAll () {
         return ResponseEntity.ok().body(categoryService.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> findById (@PathVariable Long id) {
+        return ResponseEntity.ok().body(categoryService.findById(id));
+    }
+
 }
--- backend/src/main/java/com/example/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/example/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.example.dscatalog.services;
 import com.example.dscatalog.dtos.CategoryDto;
 import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.repositories.CategoryRepository;
+import com.example.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,18 @@ import java.util.List;
 public class CategoryService {
 
     @Autowired
-    private CategoryRepository categoryRepsitory;
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public List<CategoryDto> findAll () {
-        List<Category> list = categoryRepsitory.findAll();
+        List<Category> list = categoryRepository.findAll();
         return list.stream().map(cat -> new CategoryDto(cat)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDto findById (Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
+        return new CategoryDto(category);
     }
 
 }
